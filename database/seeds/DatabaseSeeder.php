a9bb15d56efb3fe6d48f1c9481988311a3879b5a
Adam Marshall <adam@appsynergy.net>
2018-08-08 10:31:49 +0100

    Minimal seeds.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,15 +16,15 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             SpecialistSeeder::class,
             FactionSeeder::class,
-            AdeptusMechanicusSeeder::class,
+            //AdeptusMechanicusSeeder::class,
         ]);
 
         //factory(App\Models\Ability::class, 5)->create();
         //factory(App\Models\Keyword::class, 10)->create();
         //factory(App\Models\Datasheet::class, 10)->create();
         //factory(App\Models\Miniature::class, 10)->create();
-        factory(App\Models\Killteam::class, 10)->create();
-        factory(App\Models\Fighter::class, 10)->create();
+        //factory(App\Models\Killteam::class, 10)->create();
+        //factory(App\Models\Fighter::class, 10)->create();
 
 
 
