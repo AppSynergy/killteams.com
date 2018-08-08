03aba7745601c1bd1f466e1093a7b320c7e2cd84
Adam Marshall <adam@appsynergy.net>
2018-08-08 09:41:53 +0100

    Menu tidyup

---
+++ config/backpack/menu.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'items' => [
+        'General' => [
+            'user' => [
+                'icon' => 'fa-tag',
+                'name' => 'Users',
+            ]
+        ],
+        'Definitions' => [
+            'faction' => [
+                'icon' => 'fa-tag',
+                'name' => 'Factions',
+            ],
+            'datasheet' => [
+                'icon' => 'fa-tag',
+                'name' => 'Datasheets',
+            ],
+            'miniature' => [
+                'icon' => 'fa-tag',
+                'name' => 'Miniatures',
+            ],
+            'wargearoption' => [
+                'icon' => 'fa-tag',
+                'name' => 'Wargear Options',
+            ],
+            'specialism' => [
+                'icon' => 'fa-tag',
+                'name' => 'Specialisms',
+            ],
+            'keyword' => [
+                'icon' => 'fa-tag',
+                'name' => 'Keywords',
+            ],
+            'ability' => [
+                'icon' => 'fa-tag',
+                'name' => 'Abilities',
+            ],
+        ],
+        'Kill Teams' => [
+            'killteam' => [
+                'icon' => 'fa-tag',
+                'name' => 'Kill Teams',
+            ],
+            'fighter' => [
+                'icon' => 'fa-tag',
+                'name' => 'Fighters',
+            ],
+        ],
+    ],
+];
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,66 +1,22 @@
+
+
 <li>
     <a href="{{ backpack_url('dashboard') }}">
         <i class="fa fa-dashboard"></i>
         <span>{{ trans('backpack::base.dashboard') }}</span>
     </a>
 </li>
-<li>
-    <a href='{{ backpack_url('user') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Users</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('faction') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Factions</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('datasheet') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Datasheets</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('miniature') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Miniatures</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('wargearoption') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Wargear Options</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('specialism') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Specialisms</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('keyword') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Keywords</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('ability') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Abilities</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('killteam') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Killteams</span>
-    </a>
-</li>
-<li>
-    <a href='{{ backpack_url('fighter') }}'>
-        <i class='fa fa-tag'></i>
-        <span>Fighters</span>
-    </a>
-</li>
+
+@foreach (\Config::get('backpack.menu.items') as $group => $items)
+    <li class="text-white">
+        <a class="h4">{{ $group }}</a>
+    </li>
+    @foreach ($items as $slug => $item)
+        <li>
+            <a href='{{ backpack_url($slug) }}'>
+                <i class='fa {{ $item['icon'] }}'></i>
+                <span>{{ $item['name'] }}</span>
+            </a>
+        </li>
+    @endforeach
+@endforeach
