2b22a640b1240637d467ebbac3040bdeec0e6864
Adam Marshall <adam@appsynergy.net>
2018-08-11 17:41:41 +0100

    Human-readable wargear options

--- resources/assets/js/components/Builder/WargearSelector.vue
+++ resources/assets/js/components/Builder/WargearSelector.vue
@@ -3,18 +3,45 @@
         <button type="button" class="ml-3 btn btn-sm btn-info btn-wargear"
             title="Wargear Options"
             data-toggle="popover"
-            data-html="true"
-            v-on:click="openWargearOptions()">
+            data-html="true">
             COG
         </button>
         <div class="d-none wargear-selector-list">
-            {{ fighter.wargear_options }}
+            <div class="form-check" v-for="opt in fighter.wargear_options">
+                <input type="checkbox" class="form-check-input">
+                <span v-if="'REPLACE' == opt.may">
+                    Replace {{ itemsToText(opt.replace) }} with
+                </span>
+                <span v-if="'TAKE' == opt.may">
+                    Take
+                </span>
+                <span v-if="'ALLOF' == opt.method">
+                    {{ itemsToText(opt.options) }}
+                </span>
+                <span v-if="'ONEOF' == opt.method">
+                    <select class="form-control">
+                        <option v-for="choice in opt.options" value="choice">
+                            {{ idToName(choice) }}
+                        </option>
+                    </select>
+                </span>
+                <span v-if="'ONEOFEACHOF' == opt.method"
+                    v-for="options in opt.options">
+                    <select class="form-control">
+                        <option v-for="choice in options" value="choice">
+                            {{ idToName(choice) }}
+                        </option>
+                    </select>
+                </span>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import hasFactionStore from '../../mixins/hasFactionStore.js'
 export default {
+    mixins: [ hasFactionStore ],
     props: ['fighter'],
     mounted() {
         jQuery(function () {
@@ -27,7 +54,16 @@ export default {
         })
     },
     methods: {
-        openWargearOptions() {
+        idToName(item) {
+            return _.find(this.faction.wargear, (g) => g.id == item).name.toLowerCase()
+        },
+        itemsToText(items) {
+            return this.arrayToList(_.map(items, this.idToName))
+        },
+        arrayToList(arr) {
+            return arr.join(', ').replace(/,([^,]*)$/,' and$1')
+        },
+        setFighterArmament() {
 
         }
     }
