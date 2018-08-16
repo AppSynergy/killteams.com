e788c93f9c2bdc71754bdb9c0d4f60ec77cd881c
Adam Marshall <adam@appsynergy.net>
2018-08-16 12:06:39 +0100

    Narrative config example

---
+++ config/narrative.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'name' => [
+
+        'DEATH GUARD' => [
+
+            'forename' => [
+                // from book
+                'Gurloch', 'Suppurax', 'Golchor', 'Festamus', 'Rancidius',
+                'Mulgh', 'Shugholgh', 'Bubox', 'Pustus', 'Malignus',
+                // additions
+                'Poximus', 'Miasmo', 'Grull', 'Fungor', 'Slimey',
+            ],
+
+            'surname' => [
+                // from book
+                'Urghe', 'Volghor', 'Grulgus', 'the Pestilent', 'the Reeking',
+                'the Curdled', 'Glouch', 'Muttermould', 'Thrax', 'Trudge',
+                // additions
+                'Flyblown', 'the Pus-ridden', 'Poxbloom', 'McSlimeface'
+            ],
+
+        ],
+
+    ],
+
+];
