e9e20658b952c8776e32d34641bfee5860acd260
Adam Marshall <adam@appsynergy.net>
2018-10-31 10:21:14 +0000

    Sort of useful eslint module.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/essential'
+  ],
+  rules: {
+    "extends": "plugin:vue/strongly-recommended"
+  }
+}
